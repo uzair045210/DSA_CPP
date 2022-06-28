ff7e28ee450e9ee93e16fc3c382562179e80ffb8
Mr. Uzair <62235689+uzairdev07@users.noreply.github.com>
2022-06-28 02:44:03 +0000

    initialize project

---
+++ cmake-build-debug/CMakeFiles/DSA_CPP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DSA_CPP.dir/main.cpp.o"
+  "DSA_CPP"
+  "DSA_CPP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DSA_CPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
